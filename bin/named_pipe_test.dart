ce04c519fca69c5384a7965da850c9ffc92add25
wenwen.xu <wenwen.xu@cbctech.com>
2024-08-06 17:53:03 +0800

    .

---
+++ bin/named_pipe_test.dart
@@ -0,0 +1,5 @@
+import 'package:named_pipe_test/named_pipe_test.dart' as named_pipe_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${named_pipe_test.calculate()}!');
+}
---
+++ test/named_pipe_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:named_pipe_test/named_pipe_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
